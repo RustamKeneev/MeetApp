8cd90f83dcce3d0fd20b5ecbbefb90d85d0befb7
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-04-21 13:52:42 +0600

    setting sign in and sign up activity

--- app/src/main/java/com/onlineapteka/meetapp/activities/SignInActivity.java
+++ app/src/main/java/com/onlineapteka/meetapp/activities/SignInActivity.java
@@ -2,7 +2,9 @@ package com.onlineapteka.meetapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.onlineapteka.meetapp.R;
 
@@ -12,5 +14,16 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        initViews();
+    }
+
+    private void initViews() {
+        findViewById(R.id.text_sign_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/onlineapteka/meetapp/activities/SignUpActivity.java
+++ app/src/main/java/com/onlineapteka/meetapp/activities/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.onlineapteka.meetapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.onlineapteka.meetapp.R;
 
@@ -12,5 +13,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initViews();
+    }
+
+    private void initViews() {
+        findViewById(R.id.image_back).setOnClickListener(v -> onBackPressed());
+        findViewById(R.id.text_sign_in).setOnClickListener(v -> {onBackPressed();});
     }
 }
