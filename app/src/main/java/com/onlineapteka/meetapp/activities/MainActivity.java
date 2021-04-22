6025481cc2499e77eebdb674931d19de85d53921
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-04-22 20:15:19 +0600

    setup Firebase Cloud Messaging and Firestore

--- app/src/main/java/com/onlineapteka/meetapp/activities/MainActivity.java
+++ app/src/main/java/com/onlineapteka/meetapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.onlineapteka.meetapp;
+package com.onlineapteka.meetapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.onlineapteka.meetapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/onlineapteka/meetapp/activities/SignInActivity.java
+++ app/src/main/java/com/onlineapteka/meetapp/activities/SignInActivity.java
@@ -1,14 +1,15 @@
 package com.onlineapteka.meetapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
+import android.widget.Toast;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.onlineapteka.meetapp.R;
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +20,18 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        findViewById(R.id.text_sign_up).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
-            }
-        });
+        findViewById(R.id.text_sign_up).setOnClickListener(v -> startActivity(
+                new Intent(getApplicationContext(),SignUpActivity.class)));
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user  = new HashMap<>();
+        user.put("first_name","Rustam");
+        user.put("last_name","Keneev");
+        user.put("email","rustamkeneev@gmail.com");
+        firebaseFirestore.collection("users")
+                .add(user)
+                .addOnSuccessListener(documentReference ->
+                        Toast.makeText(SignInActivity.this,"User Inserted",Toast.LENGTH_LONG).show())
+                .addOnFailureListener(e ->
+                        Toast.makeText(SignInActivity.this,"Error adding user",Toast.LENGTH_LONG).show());
     }
 }
